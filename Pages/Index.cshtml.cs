fbb1640348678e909e358e4b331110a64e0da8a1
KrzysztofPawlowski <krzysztofpeiv@o2.pl>
2023-03-27 15:53:13 +0200

    tutorial

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using FizzBuzzWeb.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace FizzBuzzWeb.Pages
 {
@@ -25,12 +27,13 @@ namespace FizzBuzzWeb.Pages
         }
         public IActionResult OnPost()
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                return Page();
+                HttpContext.Session.SetString("Data",
+                JsonConvert.SerializeObject(FizzBuzz));
+                return RedirectToPage("./SavedInSession");
             }
-            return RedirectToPage("./Privacy");
+            return Page();
         }
-
     }
 }
